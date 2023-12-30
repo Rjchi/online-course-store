8fb9beb85fb58d4b559243bac895134680cb363e
Rjchi <richardospina18@gmail.com>
2023-12-30 17:38:36 -0500

    [Feat] Inicializamos el proyecto de la tienda de cursos online

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+declare function HOMEINIT([]): any;
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'courses-online';
+  ngOnInit(): void {
+    setTimeout(() => {
+      HOMEINIT($);
+    }, 50);
+  }
 }
