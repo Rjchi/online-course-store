cda8e2bae9aa3e98cc47d13d3d64170e15dfeb43
Rjchi <richardospina18@gmail.com>
2024-04-30 16:44:00 -0500

    [Feat] Agregar servicio al modulo home

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 declare function HOMEINIT([]): any;
 declare var $: any;
+declare function countdownT():any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,6 +12,7 @@ export class HomeComponent {
   ngOnInit(): void {
     setTimeout(() => {
       HOMEINIT($);
+      countdownT();
     }, 50);
   }
 }
