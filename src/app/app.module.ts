1819dd8e8c38832fa67797af4db80852b7b48190
Rjchi <richardospina18@gmail.com>
2024-05-03 08:03:57 -0500

    [Feat] Registrar DatePipe para el formato de las fechas en el modulo Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { DatePipe } from '@angular/common';
 import { ToastNotificationsModule } from "ngx-toast-notifications"
 
 @NgModule({
@@ -17,7 +18,7 @@ import { ToastNotificationsModule } from "ngx-toast-notifications"
     BrowserAnimationsModule,
     ToastNotificationsModule.forRoot({duration: 6000, type: 'primary',position: 'top-right'}),
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
