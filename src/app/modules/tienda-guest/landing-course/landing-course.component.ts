8682565b940245fe0e0176448c866d09861814fd
Rjchi <richardospina18@gmail.com>
2024-05-06 10:02:03 -0500

    [Feat] Agregar home init al landing

--- src/app/modules/tienda-guest/landing-course/landing-course.component.ts
+++ src/app/modules/tienda-guest/landing-course/landing-course.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+declare function HOMEINIT([]): any;
+declare var $: any;
 @Component({
   selector: 'app-landing-course',
   templateUrl: './landing-course.component.html',
-  styleUrls: ['./landing-course.component.css']
+  styleUrls: ['./landing-course.component.css'],
 })
 export class LandingCourseComponent {
+  constructor() {}
 
+  ngOnInit(): void {
+    setTimeout(() => {
+      HOMEINIT($);
+    }, 50);
+  }
 }
