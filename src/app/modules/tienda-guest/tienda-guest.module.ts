6cf4229d1b8f99c59bd2649d87545d27b207d0c2
Rjchi <richardospina18@gmail.com>
2024-05-06 09:24:13 -0500

    [Feat] Agregar modulo tienda guest

---
+++ src/app/modules/tienda-guest/tienda-guest.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TiendaGuestRoutingModule } from './tienda-guest-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TiendaGuestRoutingModule
+  ]
+})
+export class TiendaGuestModule { }
