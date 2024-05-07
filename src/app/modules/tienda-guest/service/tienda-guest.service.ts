7c8141daa1f63b54030a188eb03b1b83883f991f
Rjchi <richardospina18@gmail.com>
2024-05-07 10:47:04 -0500

    [Feat] Agregar consulta a un curso en especifico

--- src/app/modules/tienda-guest/service/tienda-guest.service.ts
+++ src/app/modules/tienda-guest/service/tienda-guest.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { AuthService } from '../../auth/service/auth.service';
+import { URL_SERVICIOS } from 'src/app/config/config';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TiendaGuestService {
+  constructor(public http: HttpClient, public authService: AuthService) {}
 
-  constructor() { }
+  showCourse(slug: any, time_now = null) {
+    let URL = URL_SERVICIOS + 'home/landig-course/' + slug;
+
+    return this.http.get(URL);
+  }
 }
