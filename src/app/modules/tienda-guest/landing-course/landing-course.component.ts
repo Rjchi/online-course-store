64f22e8013c925af5c936e7d88c3501129b1b98b
Rjchi <richardospina18@gmail.com>
2024-05-07 10:46:31 -0500

    [Feat]

--- src/app/modules/tienda-guest/landing-course/landing-course.component.ts
+++ src/app/modules/tienda-guest/landing-course/landing-course.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { TiendaGuestService } from '../service/tienda-guest.service';
 
 declare function HOMEINIT([]): any;
 declare var $: any;
@@ -8,9 +11,24 @@ declare var $: any;
   styleUrls: ['./landing-course.component.css'],
 })
 export class LandingCourseComponent {
-  constructor() {}
+  course: any;
+  slug: string = '';
+
+  constructor(
+    public tiendaGuestService: TiendaGuestService,
+    public activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: any) => {
+      this.slug = params.slug;
+    });
+
+    this.tiendaGuestService.showCourse(this.slug).subscribe((response: any) => {
+      console.log(response);
+      this.course = response.course;
+    });
+
     setTimeout(() => {
       HOMEINIT($);
     }, 50);
