31fb996778b9d8c572ac5ca45f55960a493944fe
Rjchi <richardospina18@gmail.com>
2024-05-15 09:59:40 -0500

    [Feat] Agregamos el observable del carrito de compras al header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { AuthService } from 'src/app/modules/auth/service/auth.service';
+import { CartService } from 'src/app/modules/home/service/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +10,32 @@ import { AuthService } from 'src/app/modules/auth/service/auth.service';
 })
 export class HeaderComponent {
   user: any = null;
+  carts: any = [];
+  sum_total: number = 0;
 
-  constructor(public authService: AuthService) {
+  constructor(
+    public authService: AuthService,
+    public cartService: CartService
+  ) {
     this.user = this.authService.user;
+  }
+
+  ngOnInit() {
+    /**--------------------------
+     * | Llamamos al observador
+     * --------------------------*/
+    this.cartService.currentData$.subscribe((response: any) => {
+      console.log("header", response);
+      this.carts = response;
+      /**-----------------------------------------------------------------------
+       * | reduce(acumulador, iterable) => acumulador + iterable, valorInicial
+       * -----------------------------------------------------------------------*/
+      this.sum_total = this.carts.reduce(
+        (accumulator: number, currentValue: any) =>
+          accumulator + currentValue.total,
+        0
+      );
+    });
   }
 
   logout() {
