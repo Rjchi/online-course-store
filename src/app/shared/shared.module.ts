f97cb54238e2a8066da0218e3ed4ee5dc179c5ef
Rjchi <richardospina18@gmail.com>
2024-05-15 11:17:35 -0500

    [Feat] Agregar importaciones principales para el modulo shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [
+    FormsModule,
+    CommonModule,
+    RouterModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
   exports: [HeaderComponent, FooterComponent],
 })
 export class SharedModule {}
