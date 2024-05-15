7287be3778a46b56e17fe4540224d1b0ddd1f503
Rjchi <richardospina18@gmail.com>
2024-05-15 09:36:23 -0500

    [Feat] Agregar enpoints al servicio de cart

--- src/app/modules/home/service/cart.service.ts
+++ src/app/modules/home/service/cart.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { AuthService } from '../../auth/service/auth.service';
+import { URL_SERVICIOS } from 'src/app/config/config';
 
 @Injectable({
   providedIn: 'root',
@@ -57,5 +58,44 @@ export class CartService {
     }
 
     this.cart.next(listCart);
+  }
+
+  /**------------
+   * | Enpoints
+   * ------------*/
+  listCart() {
+    let URL = URL_SERVICIOS + 'cart/list';
+    let headers = new HttpHeaders({
+      token: this.authService.token,
+    });
+
+    return this.http.get(URL, { headers });
+  }
+
+  registerCart(data: any) {
+    let URL = URL_SERVICIOS + 'cart/register';
+    let headers = new HttpHeaders({
+      token: this.authService.token,
+    });
+
+    return this.http.post(URL, data, { headers });
+  }
+
+  deleteCart(id: any) {
+    let URL = URL_SERVICIOS + 'cart/remove/' + id;
+    let headers = new HttpHeaders({
+      token: this.authService.token,
+    });
+
+    return this.http.delete(URL, { headers });
+  }
+
+  applyCupon(data: any) {
+    let URL = URL_SERVICIOS + 'cart/update';
+    let headers = new HttpHeaders({
+      token: this.authService.token,
+    });
+
+    return this.http.put(URL, data, { headers });
   }
 }
