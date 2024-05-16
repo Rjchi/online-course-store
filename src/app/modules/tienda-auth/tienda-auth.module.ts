d4b82724182639e578e2a1cdadd9f9bd0a6961a8
Rjchi <richardospina18@gmail.com>
2024-05-16 09:43:18 -0500

    [Feat] Agregar importaciones genericas al modulo tienda auth

--- src/app/modules/tienda-auth/tienda-auth.module.ts
+++ src/app/modules/tienda-auth/tienda-auth.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { CartsComponent } from './carts/carts.component';
-import { TiendaAuthRoutingModule } from './tienda-auth-routing.module';
 import { TiendaAuthComponent } from './tienda-auth.component';
-
+import { TiendaAuthRoutingModule } from './tienda-auth-routing.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +16,13 @@ import { TiendaAuthComponent } from './tienda-auth.component';
   ],
   imports: [
     CommonModule,
-    TiendaAuthRoutingModule
+    TiendaAuthRoutingModule,
+
+    FormsModule,
+    SharedModule,
+    RouterModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ]
 })
 export class TiendaAuthModule { }
