55b346a5d2fdbcfc7805ab9c2650bc92d6d84b52
Rjchi <richardospina18@gmail.com>
2024-05-16 08:51:52 -0500

    [Feat] Agregar componente carts

--- src/app/modules/tienda-auth/tienda-auth.module.ts
+++ src/app/modules/tienda-auth/tienda-auth.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TiendaAuthRoutingModule } from './tienda-auth-routing.module';
+import { CartsComponent } from './carts/carts.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CartsComponent
+  ],
   imports: [
     CommonModule,
     TiendaAuthRoutingModule
