3d89682c47a31d2e0441ea483445688d5a2d1f17
Rjchi <richardospina18@gmail.com>
2024-05-16 09:52:16 -0500

    [Feat] Agregar rutas principales para el modulo tienda auth

--- src/app/modules/tienda-auth/tienda-auth-routing.module.ts
+++ src/app/modules/tienda-auth/tienda-auth-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CartsComponent } from './carts/carts.component';
+import { TiendaAuthComponent } from './tienda-auth.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TiendaAuthComponent,
+    children: [
+      {
+        path: 'shopping-cart',
+        component: CartsComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class TiendaAuthRoutingModule { }
+export class TiendaAuthRoutingModule {}
