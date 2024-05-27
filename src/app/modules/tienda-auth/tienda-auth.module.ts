09242935c2c05af3ab4d3fc38ee9ec5c751f15f1
Rjchi <richardospina18@gmail.com>
2024-05-27 16:24:23 -0500

    [Feat] Agregar componente para el dashboard del estudiante

--- src/app/modules/tienda-auth/tienda-auth.module.ts
+++ src/app/modules/tienda-auth/tienda-auth.module.ts
@@ -8,11 +8,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CartsComponent } from './carts/carts.component';
 import { TiendaAuthComponent } from './tienda-auth.component';
 import { TiendaAuthRoutingModule } from './tienda-auth-routing.module';
+import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
 
 @NgModule({
   declarations: [
     CartsComponent,
-    TiendaAuthComponent
+    TiendaAuthComponent,
+    StudentDashboardComponent
   ],
   imports: [
     CommonModule,
