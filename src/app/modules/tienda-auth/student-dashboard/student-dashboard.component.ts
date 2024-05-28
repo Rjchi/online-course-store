8def9864a12fc70c4be47fddb921a6ff33c5fdb0
Rjchi <richardospina18@gmail.com>
2024-05-28 09:34:51 -0500

    [Feat] Agregar consulta para el perfil de un estudiante

--- src/app/modules/tienda-auth/student-dashboard/student-dashboard.component.ts
+++ src/app/modules/tienda-auth/student-dashboard/student-dashboard.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+import { TiendaAuthService } from '../service/tienda-auth.service';
+
 @Component({
   selector: 'app-student-dashboard',
   templateUrl: './student-dashboard.component.html',
-  styleUrls: ['./student-dashboard.component.css']
+  styleUrls: ['./student-dashboard.component.css'],
 })
 export class StudentDashboardComponent {
+  navOpt: number = 1;
 
+  constructor(public tiendaAuthService: TiendaAuthService) {}
+
+  ngOnInit(): void {
+    this.tiendaAuthService.profileStudent().subscribe((response: any) => {
+      console.log(response);
+    });
+  }
+
+  navOption(option: number) {
+    this.navOpt = option;
+  }
+
+  logout() {
+    console.log("logout");
+  }
 }
