3749de33268f84bad8cafe517f33ac2b09734644
Rjchi <richardospina18@gmail.com>
2024-05-28 09:55:38 -0500

    [Feat] Agregar conteo de los cursos activos, terminados y sin iniciar

--- src/app/modules/tienda-auth/student-dashboard/student-dashboard.component.ts
+++ src/app/modules/tienda-auth/student-dashboard/student-dashboard.component.ts
@@ -10,11 +10,18 @@ import { TiendaAuthService } from '../service/tienda-auth.service';
 export class StudentDashboardComponent {
   navOpt: number = 1;
 
+  actived_course_count: number = 0;
+  enrolled_course_count: number = 0;
+  termined_course_count: number = 0;
+
   constructor(public tiendaAuthService: TiendaAuthService) {}
 
   ngOnInit(): void {
     this.tiendaAuthService.profileStudent().subscribe((response: any) => {
       console.log(response);
+      this.actived_course_count = response.activated_course_count;
+      this.enrolled_course_count = response.enrolled_course_count;
+      this.termined_course_count = response.termined_course_count;
     });
   }
 
@@ -23,6 +30,6 @@ export class StudentDashboardComponent {
   }
 
   logout() {
-    console.log("logout");
+    console.log('logout');
   }
 }
