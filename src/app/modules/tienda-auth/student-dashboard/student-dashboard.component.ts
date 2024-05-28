e198823db5a14d5fa6bbd3f364987071fb8c8190
Rjchi <richardospina18@gmail.com>
2024-05-28 10:32:02 -0500

    [Feat] Agregar el detallado de los cursos

--- src/app/modules/tienda-auth/student-dashboard/student-dashboard.component.ts
+++ src/app/modules/tienda-auth/student-dashboard/student-dashboard.component.ts
@@ -16,6 +16,10 @@ export class StudentDashboardComponent {
 
   profile: any = null;
 
+  actived_course_news: any = null;
+  termined_course_news: any = null;
+  enrolled_course_news: any = null;
+
   constructor(public tiendaAuthService: TiendaAuthService) {}
 
   ngOnInit(): void {
@@ -26,6 +30,10 @@ export class StudentDashboardComponent {
       this.termined_course_count = response.termined_course_count;
 
       this.profile = response.profile;
+
+      this.actived_course_news = response.actived_course_news;
+      this.termined_course_news = response.termined_course_news;
+      this.enrolled_course_news = response.enrolled_course_news;
     });
   }
 
