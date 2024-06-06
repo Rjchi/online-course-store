a599444cc49323c4b08c6e5c8f05e875d14db9e2
Rjchi <richardospina18@gmail.com>
2024-06-06 13:34:40 -0500

    [Feat] Agrear el listado de los cursos filtrados

--- src/app/modules/tienda-guest/filters-course/filters-course.component.ts
+++ src/app/modules/tienda-guest/filters-course/filters-course.component.ts
@@ -9,7 +9,10 @@ declare var $: any;
   styleUrls: ['./filters-course.component.css'],
 })
 export class FiltersCourseComponent {
+  select_option: number = 1;
+
   levels: any = [];
+  courses: any = [];
   idiomas: any = [];
   categories: any = [];
   instructores: any = [];
@@ -24,22 +27,38 @@ export class FiltersCourseComponent {
       this.instructores = response.instructores;
     });
 
-    setTimeout(() => {
-      $('#slider-range').slider({
-        range: true,
-        min: 10,
-        max: 500,
-        values: [100, 300],
-        slide: function (event: any, ui: any) {
-          $('#amount').val('$' + ui.values[0] + ' - $' + ui.values[1]);
-        },
-      });
-      $('#amount').val(
-        '$' +
-          $('#slider-range').slider('values', 0) +
-          ' - $' +
-          $('#slider-range').slider('values', 1)
-      );
-    }, 50);
+    this.filterCourses();
+  }
+
+  addOption(option: number) {
+    this.select_option = option;
+
+    if (this.select_option === 2) {
+      setTimeout(() => {
+        $('#slider-range').slider({
+          range: true,
+          min: 10,
+          max: 500,
+          values: [100, 300],
+          slide: function (event: any, ui: any) {
+            $('#amount').val('$' + ui.values[0] + ' - $' + ui.values[1]);
+          },
+        });
+        $('#amount').val(
+          '$' +
+            $('#slider-range').slider('values', 0) +
+            ' - $' +
+            $('#slider-range').slider('values', 1)
+        );
+      }, 50);
+    }
+  }
+
+  filterCourses() {
+    let data = {};
+
+    this.tiendaGuestService.searchCourse(data).subscribe((response: any) => {
+      this.courses = response.courses;
+    });
   }
 }
