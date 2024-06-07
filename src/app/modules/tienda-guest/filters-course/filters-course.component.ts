abcf97fd409795daa5fbd13dec2562b4d0e4cebc
Rjchi <richardospina18@gmail.com>
2024-06-07 07:44:27 -0500

    [Feat] Agregar opción de agregar un curso al carrito de compras

--- src/app/modules/tienda-guest/filters-course/filters-course.component.ts
+++ src/app/modules/tienda-guest/filters-course/filters-course.component.ts
@@ -1,5 +1,7 @@
+import { Toaster } from 'ngx-toast-notifications';
 import { Component } from '@angular/core';
 
+import { CartService } from '../../home/service/cart.service';
 import { TiendaGuestService } from '../service/tienda-guest.service';
 
 declare var $: any;
@@ -11,15 +13,23 @@ declare var $: any;
 export class FiltersCourseComponent {
   select_option: number = 1;
 
+  user: any;
   levels: any = [];
   courses: any = [];
   idiomas: any = [];
   categories: any = [];
   instructores: any = [];
 
-  constructor(public tiendaGuestService: TiendaGuestService) {}
+  constructor(
+    public toaster: Toaster,
+    public cartService: CartService,
+    public tiendaGuestService: TiendaGuestService,
+  ) {}
 
   ngOnInit(): void {
+    window.scroll(0, 0);
+    this.user = this.cartService.authService.user;
+
     this.tiendaGuestService.getConfigAll().subscribe((response: any) => {
       this.levels = response.levels;
       this.idiomas = response.idiomas;
@@ -59,6 +69,75 @@ export class FiltersCourseComponent {
 
     this.tiendaGuestService.searchCourse(data).subscribe((response: any) => {
       this.courses = response.courses;
+    });
+  }
+
+  getNewTotal(course: any, campaing_banner: any) {
+    if (campaing_banner.type_discount === 1) {
+      return Math.round(
+        course.price_usd - course.price_usd * (campaing_banner.discount * 0.01)
+      );
+    } else {
+      return Math.round(course.price_usd - campaing_banner.discount);
+    }
+  }
+
+  getTotalPriceCourse(course: any) {
+    if (course.discount_g) {
+      return this.getNewTotal(course, course.discount_g);
+    } else {
+      return course.price_usd;
+    }
+  }
+
+  addCart(course: any, campaing: any = null) {
+    if (!this.user) {
+      this.toaster.open({
+        text: 'NECESITAS INGRESAR CON TU CUENTA AL SISTEMA',
+        caption: 'VALIDATIONS',
+        type: 'danger',
+      });
+      this.cartService.authService.router.navigateByUrl('auth/login');
+
+      return;
+    }
+
+    if (campaing) {
+      course.discount_g = campaing;
+    }
+
+    let data = {
+      total: this.getTotalPriceCourse(course),
+      course: { _id: course._id },
+      discount: course.discount_g.discount ? course.discount_g.discount : null,
+      subtotal: this.getTotalPriceCourse(course),
+      price_unit: course.price_usd,
+      code_cupon: null,
+      type_discount: course.discount_g.type_discount
+        ? course.discount_g.type_discount
+        : null,
+      code_discount: course.discount_g._id ? course.discount_g._id : null,
+      campaing_discount: course.discount_g.type_campaing
+        ? course.discount_g.type_campaing
+        : null,
+    };
+
+    this.cartService.registerCart(data).subscribe((response: any) => {
+      if (response.message && response.message === 403) {
+        this.toaster.open({
+          text: response.message_text,
+          caption: 'VALIDATIONS',
+          type: 'danger',
+        });
+      } else {
+        this.cartService.addCart(response.cart);
+
+        this.toaster.open({
+          text: response.message_text,
+          caption: 'VALIDATIONS',
+          type: 'primary',
+        });
+      }
     });
   }
 }
