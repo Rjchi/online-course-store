817f2774c25f09ad7149bfae2115e261ba0b3d8c
Rjchi <richardospina18@gmail.com>
2024-06-11 08:59:12 -0500

    [Feat] Traer el curso seleccionado por el usuario

--- src/app/modules/tienda-auth/course-leason/course-leason.component.ts
+++ src/app/modules/tienda-auth/course-leason/course-leason.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+import { TiendaAuthService } from '../service/tienda-auth.service';
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-course-leason',
   templateUrl: './course-leason.component.html',
-  styleUrls: ['./course-leason.component.css']
+  styleUrls: ['./course-leason.component.css'],
 })
 export class CourseLeasonComponent {
+  slug: string = '';
 
+  constructor(
+    public tiendaAuthService: TiendaAuthService,
+    public activatedRouter: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRouter.params.subscribe((params: any) => {
+      this.slug = params.slug;
+    });
+
+    this.tiendaAuthService
+      .courseLeason(this.slug)
+      .subscribe((response: any) => {
+        console.log(response);
+      });
+  }
 }
