11a776fa0cf4fcaa8e6a92c9c28bdcdb70b75d22
Rjchi <richardospina18@gmail.com>
2024-06-11 08:55:10 -0500

    [Feat] Agregar función para traer el detalle de un curso donde el usuario este inscrito

--- src/app/modules/tienda-auth/service/tienda-auth.service.ts
+++ src/app/modules/tienda-auth/service/tienda-auth.service.ts
@@ -44,4 +44,11 @@ export class TiendaAuthService {
 
     return this.http.post(url, data, { headers });
   }
+
+  courseLeason(slug: string) {
+    let url = URL_SERVICIOS + 'profile/course/' + slug;
+    let headers = new HttpHeaders({ token: this.authService.token });
+
+    return this.http.get(url, { headers });
+  }
 }
