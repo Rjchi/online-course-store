6ed79f55eabb2a0c3523f23b66bf97bf9ba9f67b
Rjchi <richardospina18@gmail.com>
2024-06-17 09:41:57 -0500

    [Chore] Indicamos que en la configuración de producción se reemplace el archivo de donde tomamos variables de entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,14 @@
-export const environment = {};
+export const environment = {
+  /**----------------------------------------------------------------------------------------
+   * | Aqui debemos indicar los dominios reales que vamos a utilizar en la aplicación
+   * | una vez modificados aqui los valores debemos indicar a angular que tome los valores
+   * | de este archivo en vez de los de desarrollo esto se hace en el archivo angular.json
+   * | en el apartado de 'configurations: {...}'
+   * ----------------------------------------------------------------------------------------*/
+
+  URL_BACKEND: 'http://127.0.0.1:3000/',
+  URL_SERVICIOS: 'http://127.0.0.1:3000/api/',
+  URL_FROTEND: 'http://localhost:4200',
+  CLIENT_ID:
+    'ASN9cNwo9Zdu1D9PRorW9SZ4E707kBU8JpQ7h7iFM2tMTYBVczypQrz5Ne18prx4zdvs09bqPgZ_uUU1',
+};
